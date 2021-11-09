71279bf6772ab63facdde61e95401a5bb4a27165
liantus5 <lianaginiat@gmail.com>
2021-11-08 22:05:16 -0800

    created a header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React from "react";
 
-export default function Header (){
-    return <div> 
-        hello
+export default function Header() {
+  return (
+    <div className="Header container">
+      <div className="row">
+        <h2> Front-end developer</h2>
+        <h1>Liana Giniatullina</h1>
+      </div>
     </div>
+  );
 }
