bee3998b057d820dbd06ba639126cc23f978d207
liantus5 <lianaginiat@gmail.com>
2021-12-06 20:28:43 -0800

    made website responsive

--- src/Project.js
+++ src/Project.js
@@ -3,15 +3,15 @@ import React from "react";
 export default function Project(props) {
   return (
     <div className="row p-3">
-      <div className="col-sm-6">
+      <div className="col-lg-6">
         <img
           src={props.image}
           alt={props.title}
           className="img-fluid rounded shadow"
         />
       </div>
-      <div className="col-sm-6 align-self-center">
-        <h2 className="mb-4">{props.title}</h2>
+      <div className="col-lg-6 align-self-center">
+        <h2 className="mb-4 mt-4">{props.title}</h2>
         <div className="mb-5 ps-5 pe-5">{props.text}</div>
         <a
           href={props.link}
